dfc6035261cdb85614a793a0f0c179edb82b008b
Michal Pekar <pekac45@gmail.com>
2019-04-20 23:07:47 +0100

    404 finished

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,29 @@
+/* eslint-disable no-restricted-globals */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import get from 'lodash/get';
 import Template from '../components/layout';
 
 export class PageNotFound extends Component {
   render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
+    const siteTitle = '404 | Henrik Salomon';
 
     return (
       <div>
         <Template>
           <Helmet title={siteTitle} />
-          <h2>404</h2>
+          <div className="row">
+            <div
+              className="col-lg-12 col-md-12 widget text-center u-mar-top-medium u-mar-bot-medium
+"
+            >
+              <h2>404</h2>
+              <h4>
+                No match found for
+                {` ${location.pathname}`}
+              </h4>
+            </div>
+          </div>
         </Template>
       </div>
     );
