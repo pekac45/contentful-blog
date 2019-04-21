d79dd39ee72d3dd40a97e956730a1ea724a2c97c
Michal Pekar <pekac45@gmail.com>
2019-04-21 22:50:59 +0100

    picutre smaller, amend 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,33 +1,34 @@
 /* eslint-disable no-restricted-globals */
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
+import React from 'react';
 import Helmet from 'react-helmet';
 import Template from '../components/layout';
 
-export class PageNotFound extends Component {
-  render() {
-    const siteTitle = '404 | Henrik Salomon';
+const PageNotFound = () => {
+  const siteTitle = '404 | Henrik Salomon';
 
-    return (
-      <div>
-        <Template>
-          <Helmet title={siteTitle} />
-          <div className="row">
-            <div
-              className="col-lg-12 col-md-12 widget text-center u-mar-top-medium u-mar-bot-medium
-"
-            >
-              <h2>404</h2>
-              <h4>
-                No match found for
-                {` ${location.pathname}`}
-              </h4>
-            </div>
+  return (
+    <div>
+      <Template>
+        <Helmet title={siteTitle} />
+        <div className="row">
+          <div
+            className="col-lg-12
+            col-md-12
+            widget
+            text-center
+            u-mar-top-medium
+            u-mar-bot-medium"
+          >
+            <h2>404</h2>
+            <h4>
+              No match found for
+              {` ${location.pathname}`}
+            </h4>
           </div>
-        </Template>
-      </div>
-    );
-  }
-}
+        </div>
+      </Template>
+    </div>
+  );
+};
 
 export default PageNotFound;
