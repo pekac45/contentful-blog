04c2a421a71a246047f85986919ebb9438e99ed5
Michal Pekar <pekac45@gmail.com>
2019-04-25 21:38:12 +0100

    hero responsive

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,13 +5,11 @@ import heroImg from '../images/hero.webp';
 const Hero = () => {
   return (
     <section className="hero-wrapper pad-50 bg-lightblue style-2">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 col-md-12 widget text-center hero-content">
-            <img src={heroImg} alt="Henrik Salomon faceshot" className="hero-picture" />
-            <h1>Henrik Salomon</h1>
-            <h3>Future R&B Producer</h3>
-          </div>
+      <div className="container-fluid">
+        <div className="widget text-center hero-content">
+          <img src={heroImg} alt="Henrik Salomon faceshot" className="hero-picture" />
+          <h1>Henrik Salomon</h1>
+          <h3>Future R&B Producer</h3>
         </div>
       </div>
     </section>
